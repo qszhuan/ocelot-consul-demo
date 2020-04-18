f2ebd7d89feb22b9d95e52978987decd3d5c149b
Qingshan Zhuan <qingshan.zhuan@fabricgroup.com.au>
2020-04-18 18:26:50 +1000

    ocelet demo for gateway

---
+++ ocelot-demo/order-api/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace order_api.Controllers
+{
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [Route("api/orders")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[]
+            {
+                "Order 1",
+                "Order 2"
+            };
+        }
+    }
+}
---
+++ ocelot-demo/product-api/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace product_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[]{"Laptop", "Keyboard"};
+        }
+    }
+}
